1ac6a6a7d040858a0cb8e9d5a11d92fff9416c1f
otti88 <otti.contra@googlemail.com>
2021-05-29 13:03:47 +0200

    Style menu

--- src/component/layout.js
+++ src/component/layout.js
@@ -1,21 +1,20 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { menubar, menuitem, menuitemlink } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <main>
         <title>{pageTitle}</title>
         <h2>Alexs Camper</h2>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/infos">Informationen</Link></li>
-                <li><Link to="/galery">Galerie</Link></li>
-                <li><Link to="/calendar">Buchen</Link></li>
-                <li><Link to="/terms">Mietbedingungen</Link></li>
-            </ul>
-        </nav>
-        <h1>{pageTitle}</h1>
+        <ul className={menubar} >
+            <li className={menuitem}><Link className={menuitemlink} to="/">Home</Link></li>
+            <li className={menuitem}><Link className={menuitemlink} to="/infos">Informationen</Link></li>
+            <li className={menuitem}><Link className={menuitemlink} to="/galery">Galerie</Link></li>
+            <li className={menuitem}><Link className={menuitemlink} to="/calendar">Buchen</Link></li>
+            <li className={menuitem}><Link className={menuitemlink} to="/terms">Mietbedingungen</Link></li>
+        </ul>
+        <h3>{pageTitle}</h3>
         {children}
         <p>Impressum und Kontakt</p>
     </main>
--- src/pages/galery.js
+++ src/pages/galery.js
@@ -5,6 +5,32 @@ const GaleryPage = () => {
   return (
     <Layout pageTitle="Galerie">
         <p>Hier kommen Bilder und son kram hin.</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
+        <p>bild</p>
     </Layout>
   )
 }
